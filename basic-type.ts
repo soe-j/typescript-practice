e19d50bed83afe6114be5c3cdc2490a66824fde3
soe-j <7115203+soe-j@users.noreply.github.com>
2020-05-20 12:16:05 +0900

    practice: basic type

---
+++ basic-type.ts
@@ -0,0 +1,16 @@
+// function loop() : never {
+//   while (true) {
+
+//   }
+// }
+
+// loop()
+
+let someValue1: any = "hey"
+console.log(someValue1.length)
+
+let someValue2: any = 1
+console.log(someValue2.length)
+
+let someValue3: any = 1
+console.log(someValue3.length)
